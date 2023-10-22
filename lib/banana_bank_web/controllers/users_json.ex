7017f1563e3c6f1c7f392bb2ea387f797706c7b1
Jessé Augusto <jessealvessilva@gmail.com>
2023-10-22 16:31:25 -0300

    Criação de função axiliar para a exibição dos dados

--- lib/banana_bank_web/controllers/users_json.ex
+++ lib/banana_bank_web/controllers/users_json.ex
@@ -1,11 +1,19 @@
 defmodule BananaBankWeb.UsersJSON do
 
+  alias BananaBank.Users.User
+
   def create(%{user: user}) do
     %{
       messsege: "User criado com sucesso!",
-      id: user.id,
-      cep: user.cep,
-      cep: user.email
+      data: data(user)
     }
+  end
+
+  def data( %User{} = user ) do
+     %{
+        id: user.id,
+        cep: user.cep,
+        email: user.email
+     }
   end
 end
