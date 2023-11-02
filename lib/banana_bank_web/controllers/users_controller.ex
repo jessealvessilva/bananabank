628088c6d32ef0c90f3e1b002f4260c2df1e6f5e
Jessé Augusto <jessealvessilva@gmail.com>
2023-11-02 17:01:33 -0300

    Math com with

--- lib/banana_bank_web/controllers/users_controller.ex
+++ lib/banana_bank_web/controllers/users_controller.ex
@@ -2,30 +2,29 @@ defmodule BananaBankWeb.UsersController do
   use BananaBankWeb, :controller
 
   alias BananaBank.Users.Create
+  alias BananaBank.Users.User
 
   def create(conn, params) do
+    with {:ok, %User{} = user } <- Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, user: user)
+    end
 
-    # conn
-    # |> put_status(:ok)
-    # |> json("Bem vindo ao BananaBank")
-
-    params
-    |> Create.call()
-    |> handle_response(conn)
   end
 
-  defp handle_response( {:ok, user }, conn ) do
+  # defp handle_response( {:ok, user }, conn ) do
 
-    conn
-    |> put_status(:created)
-    |> render(:create, user: user)
-  end
+  #   conn
+  #   |> put_status(:created)
+  #   |> render(:create, user: user)
+  # end
 
-  defp handle_response({:error, changeset }, conn ) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(json: BananaBankWeb.ErrorJSON)
-    |> render(:error, changeset: changeset)
-  end
+  # defp handle_response({:error, changeset }, conn ) do
+  #   conn
+  #   |> put_status(:bad_request)
+  #   |> put_view(json: BananaBankWeb.ErrorJSON)
+  #   |> render(:error, changeset: changeset)
+  # end
 
 end
