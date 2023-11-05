c29c9a36d7eb8bb626bb9224eefcef5559ba2b8c
Jessé Augusto <jessealvessilva@gmail.com>
2023-11-05 13:20:34 -0300

    Gerenciamento de erro no get

--- lib/banana_bank/users/get.ex
+++ lib/banana_bank/users/get.ex
@@ -5,7 +5,7 @@ defmodule BananaBank.Users.Get do
 
   def call(id) do
     case Repo.get(User, id ) do
-      nil  -> {:error, :not_founf}
+      nil  -> {:error, :not_found}
       user -> {:ok, user}
     end
   end
--- lib/banana_bank_web/controllers/fallback_controller.ex
+++ lib/banana_bank_web/controllers/fallback_controller.ex
@@ -4,11 +4,17 @@ defmodule BananaBankWeb.FallbackController do
   alias BananaBank.Users.Create
   alias BananaBank.Users.User
 
+  def call(conn, {:error, :not_found }) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(json: BananaBankWeb.ErrorJSON)
+    |> render(:error, status: :not_found)
+  end
+
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
     |> put_view(json: BananaBankWeb.ErrorJSON)
     |> render(:error, changeset: changeset)
   end
-
 end
